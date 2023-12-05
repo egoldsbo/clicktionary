132087fcb9457be0492e4eb9f5559a545f9be311
Evan <egoldsbo@gmail.com>
2023-12-05 03:40:36 -0800

    updated  a nucnj

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
     const gallery = document.getElementById("gallery");
     const videoPlayer = document.getElementById("videoPlayer");
